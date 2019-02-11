32991a39197a9d55f1c406601a6f1cee4b838f12
djc74 <davidcooper974@gmail.com>
2019-02-11 03:40:39 +0000

    Added e-safety animation

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Video, Transformation } from "cloudinary-react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,8 +9,15 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Welcome to our E-safety project</h1>
-
-    <Link to="/">GO TO THE FRONTPAGE</Link>
+    <Video
+      cloudName="big-tent"
+      publicId="Michael/e-safety"
+      style={{ maxWidth: `700px`, marginBottom: `1.45rem` }}
+      controls
+    />
+    <Link to="/" style={{ display: `block` }}>
+      <h4>Go to the front page</h4>
+    </Link>
   </Layout>
 )
 
